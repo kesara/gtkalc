48629fb98bd0bbf523ac62412a7d82a67da55912
Kesara Rathnayake <kesara.rathnayake@gmail.com>
2012-11-19 20:07:52 +0530

    Added main program file.

--- src/GTKalc.py
+++ src/GTKalc.py
@@ -1,6 +1,6 @@
 ################################################################################
-# MainWindow.py
-# Main user interface.
+# GTKalc.py
+# GTK+ 3 based calculator program.
 #
 # Copyright (C) 2012 Kesara Nanayakkara Rathnayake
 #
@@ -22,29 +22,8 @@
 ################################################################################
 
 from gi.repository import Gtk
+from MainWindow import MainWindow
 
-class MainWindow:
-    """
-    Main user interface.
-    """
-    def __init__(self):
-        handlers = {
-                "onWindowDelete"    : Gtk.main_quit,
-                "onMenuQuit"        : Gtk.main_quit,
-                "onNumberButtonClick"     : self.number_press
-        }
-        builder = Gtk.Builder()
-        builder.add_from_file("MainWindow.glade")
-        builder.connect_signals(handlers)
-
-        self.entry = builder.get_object("entry")
-        
-        window = builder.get_object("main_window")
-        window.show_all()
-
-    def number_press(self, widget, data=None):
-        if widget.get_label == '.' and not('.' in self.entry.get_text()):
-            self.entry.set_text(self.entry.get_text() + ".0")
-        else:
-            self.entry.set_text(self.entry.get_text() + widget.get_label())
-
+if __name__ == "__main__":
+    mw = MainWindow()
+    Gtk.main()
