63497960d61a1d96379538fe07c577e0372fbfd9
Kesara Rathnayake <kesara.rathnayake@gmail.com>
2012-11-19 19:56:26 +0530

    Added licence information.

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,7 +1,24 @@
 ################################################################################
 # MainWindow.py
 # Main user interface.
-# Copyright (C) Kesara Nanayakkara Rathnayake
+#
+# Copyright (C) 2012 Kesara Nanayakkara Rathnayake
+#
+# This file is part of GTKalc.
+#
+# GTKalc is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GTKalc is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GTKalc.  If not, see <http://www.gnu.org/licenses/>.
+#
 ################################################################################
 
 from gi.repository import Gtk
